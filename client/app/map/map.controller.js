b82185c398be364e88cd9bb72980062569072a4f
Greg Fedirko <gjfedirko@gmail.com>
2014-10-20 01:15:35 -0700

    style and resize map

--- client/app/map/map.controller.js
+++ client/app/map/map.controller.js
@@ -13,7 +13,7 @@
 
 
         var width = document.getElementById('map').offsetWidth;
-        var height = width / 2;
+        var height = 400;
 
         var topo,projection,path,svg,g;
 
@@ -25,8 +25,8 @@
 
         function setup(width,height){
           projection = d3.geo.mercator()
-            .translate([(width/2), (height/2)])
-            .scale( width / 2 / Math.PI);
+            .translate([(width/2), (height/1.5)])
+            .scale( width / 3.4 / Math.PI);
 
           path = d3.geo.path().projection(projection);
 
@@ -36,6 +36,7 @@
               .call(zoom)
               .on("click", click)
               .append("g");
+
 
           g = svg.append("g");
 
@@ -51,16 +52,17 @@
 
         function draw(topo) {
 
-          svg.append("path")
-             .datum(graticule)
-             .attr("class", "graticule")
-             .attr("d", path);
+          // svg.append("path")
+          //    .datum(graticule)
+          //    .attr("class", "graticule")
+          //    .attr("d", path);
 
 
-          g.append("path")
-           .datum({type: "LineString", coordinates: [[-180, 0], [-90, 0], [0, 0], [90, 0], [180, 0]]})
-           .attr("class", "equator")
-           .attr("d", path);
+
+          // g.append("path")
+          //  .datum({type: "LineString", coordinates: [[-180, 0], [-90, 0], [0, 0], [90, 0], [180, 0]]})
+          //  .attr("class", "equator")
+          //  .attr("d", path);
 
 
           var country = g.selectAll(".country").data(topo);
@@ -70,7 +72,7 @@
               .attr("d", path)
               .attr("id", function(d,i) { return d.id; })
               .attr("title", function(d,i) { return d.properties.name; })
-              .style("fill", function(d, i) { return d.properties.color; });
+              .style("fill", 'red');
 
           //offsets for tooltips
           var offsetL = document.getElementById('map').offsetLeft+20;
@@ -91,22 +93,12 @@
                 tooltip.classed("hidden", true);
               });
 
-
-          //EXAMPLE: adding some capitals from external CSV file
-          d3.csv("data/country-capitals.csv", function(err, capitals) {
-
-            capitals.forEach(function(i){
-              addpoint(i.CapitalLongitude, i.CapitalLatitude, i.CapitalName );
-            });
-
-          });
-
         }
 
 
         function redraw() {
           width = document.getElementById('map').offsetWidth;
-          height = width / 2;
+          height = 400;
           d3.select('svg').remove();
           setup(width,height);
           draw(topo);
@@ -182,35 +174,5 @@
 
         }
     });
-
-  // MapCtrl.$inject = ['$scope', 'MapFactory'];
-
-  // function MapCtrl($scope, MapFactory) {
-    // console.log(ResolvedThings)
-
-    // var vm = this;
-    // vm.awesomeThings = ResolvedThings;
-    // vm.map = MapFactory;
-    // vm.remove = remove;
-
-    ////////////////////
-
-    // function addThing() {
-    //   if(vm.newThing === '') {
-    //     return;
-    //   }
-    //   vm.newThing = '';
-    // }
-
-    // function remove(id) {
-    //   vm.map.remove(id)
-    //     .then(function(){
-
-    //       var index = _.indexOf(vm.awesomeThings, {_id: id})
-    //       vm.awesomeThings.splice(index, 1);
-
-    //     })
-    // }
-  // }
 
 }).call(this);
