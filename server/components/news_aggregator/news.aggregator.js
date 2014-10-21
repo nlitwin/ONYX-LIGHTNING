1cad31e211dab4fcb207756cd230a668c2a81069
Greg Fedirko <gjfedirko@gmail.com>
2014-10-21 12:46:35 -0700

    create directory for aggregator functionality

---
+++ server/components/news_aggregator/news.aggregator.js
@@ -0,0 +1,54 @@
+// News Aggregator Module
+
+var FeedParser = require('feedparser');
+var request = require('request');
+var db = require('./../../api/thing/thing.controller');
+var apiMap = require('./news.source.config.js');
+var utils = require('./news.aggregator.utils.js');
+
+
+exports.fetchArticles = function(dbCallback) {
+
+  for (var key in apiMap) {
+    fetchSingleApi(apiMap[key]);
+  }
+
+  function fetchSingleApi (api) {
+    var req = request(api.url);
+    var feedparser = new FeedParser();
+
+    req.on('error', function (error) {
+      // handle any request errors
+    });
+    req.on('response', function (res) {
+      var stream = this;
+
+      if (res.statusCode != 200) return this.emit('error', new Error('Bad status code'));
+
+      stream.pipe(feedparser);
+    });
+
+
+    feedparser.on('error', function(error) {
+      // always handle errors
+    });
+    feedparser.on('readable', function() {
+      // This is where the action is!
+      var stream = this
+        , meta = this.meta // **NOTE** the "meta" is always available in the context of the feedparser instance
+        , item;
+
+      while (item = stream.read()) {
+        var newItem = {};
+        newItem.title = item.title;
+        newItem.location = getLocation();
+        newItem.info = item.summary;
+        newItem.url = item.link;
+        dbCallback(newItem);
+      }
+    });
+  }
+
+};
+
+
