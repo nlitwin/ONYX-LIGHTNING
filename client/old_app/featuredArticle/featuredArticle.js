0b855b77ab9c61b9bb324b70b0820fb3572cc768
Greg Fedirko <gfedirko@zagmail.gonzaga.edu>
2014-10-23 12:27:12 -0700

    add new app folder

---
+++ client/old_app/featuredArticle/featuredArticle.js
@@ -0,0 +1,14 @@
+// Delete me or move main.js/featuredArticle logic here?
+
+/*'use strict';
+
+angular.module('onyxLightningApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('featuredArticle', {
+        url: '/',
+        templateUrl: 'app/featuredArticle/featuredArticle.html',
+        controller: 'FeaturedArticleCtrl'
+      });
+  });
+*/
