898c43779d7a5b7c94e7c0c532e9739a67c6d190
Alejandro Z <alejandrozuccolillo@hotmail.com>
2014-10-25 19:03:27 -0700

    added links to click through news source

--- client/app/map/map.service.js
+++ client/app/map/map.service.js
@@ -76,7 +76,7 @@
       g.transition().duration(750).attr("transform",
           "translate(" + projection.translate() + ")"
           + "scale(" + .70 / Math.max((b[1][0] - b[0][0]) / width, (b[1][1] - b[0][1]) / height) + ")"
-          + "translate(" + -(b[1][0] + b[0][0]) / 2 + "," + -(b[1][1] + b[0][1]) / 1.85 + ")");
+          + "translate(" + -(b[1][0] + b[0][0]) / 2 + "," + -(b[1][1] + b[0][1]) / 1.95 + ")");
     }
 
     function draw(topo) {
